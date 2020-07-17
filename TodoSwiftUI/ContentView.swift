16326f86b3d6697d827cce06c1d7dca7e8283e33
Adie Olalekan <ugbeadie@gmail.com>
2020-07-17 20:31:03 +0100

    Initial Commit

---
+++ TodoSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoSwiftUI
+//
+//  Created by Adie Olami on 7/17/20.
+//  Copyright © 2020 Adie Olami. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
